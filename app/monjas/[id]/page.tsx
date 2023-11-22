6212e473a80c881716ddd71b740e1cf7e6d4db83
Fresco-7 <aluno221224@epad.edu.pt>
2023-11-22 19:13:37 +0000

    'fix mobile'

--- app/monjas/[id]/page.tsx
+++ app/monjas/[id]/page.tsx
@@ -2,9 +2,9 @@ import MonjasLivro from "@/components/MonjasLivro";
 export default async function MonjaLivro({ params }: { params: { id: string } }) {
     return (
       <>
-        <div className="p-10">
-          <MonjasLivro id={params.id}/>
-        </div>
+          <div className="p-10 grid grid-cols-1 gap-6">
+            <MonjasLivro id={params.id}/>
+          </div>
       </>
     )
 }
